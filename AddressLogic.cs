003dfa254c6c3d096b0f9206c213e7a7df2edaa3
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-27 12:47:28 +0530

    welcome

--- AddressLogic.cs
+++ AddressLogic.cs
@@ -9,6 +9,9 @@ namespace AddressBook
     public class AddressLogic
     {
         List<DataOfPerson> person1 = new List<DataOfPerson>();
+        Dictionary<string, List<DataOfPerson>> addressBook = new Dictionary<string, List<DataOfPerson>>();
+
+        //UC1-UC2-AddContact details
         public void addcontact()
         {
             DataOfPerson person = new DataOfPerson();
@@ -28,27 +31,27 @@ namespace AddressBook
             person.Email = Console.ReadLine();
             person1.Add(person);
         }
+        //UC3 EditContact
         public void editcontact()
         {
             DataOfPerson person = new DataOfPerson();
-
             Console.WriteLine("Enter first name");
             string firstName = Console.ReadLine();
             Console.WriteLine("Enter last name");
             string lastName = Console.ReadLine();
             foreach (var contact in person1)
-                if (person. FirstName == firstName && person.LastName == lastName)
+                if (person.FirstName == firstName && person.LastName == lastName)
                 {
-                    
-                    Console.WriteLine("PRESS1)" +
-                        "");
+                    Console.WriteLine("edit phone number");
                     Console.WriteLine("enter new phone number");
                     person.PhoneNum = Console.ReadLine();
                     person1.Add(person);
                 }
             Console.WriteLine("Contact sucessfully update");
 
+
         }
+        //UC4 Remove contact
         public void removecontact()
 
         {
@@ -62,9 +65,44 @@ namespace AddressBook
                 {
                     person1.Remove(person);
                 }
-
             Console.WriteLine("Contact is succesfully Removed");
 
         }
+        // display the list
+        public void Display()
+        {
+            foreach (var person in person1)
+            {
+                Console.WriteLine("Contact Details:" + "\n" + "FirstName: " + person.FirstName + "\n" + "LastName: " + person.LastName + "\n" + "Address: " + person.Address + "\n" + "City: " + person.City + "\n" + "PhoneNumber: " + person.PhoneNum + "\n" + "Email: " + person.Email);
+            }
+
+        }
+        //uc5
+        // add multiple data of person
+
+        public void CreateDictionary()
+        {
+            Console.WriteLine("Enter with what name you want to add in dictionary");
+            string name = Console.ReadLine();
+            addressBook.Add(name, person1);
+            person1 = new List<DataOfPerson>();
+        }
+
+        // UC6
+        // display dictionary which is base on key value
+        public void DisplayDictionary()
+        {
+            foreach (var data in addressBook)
+            {
+                Console.WriteLine(data.Key);         //printing dictionary keys
+                foreach (var person in data.Value)   // checking values inside keys
+                {
+                    Console.WriteLine("Contact Details:" + "\n" + "FirstName: " + person.FirstName + "\n" + "LastName: " + person.LastName + "\n" + "Address: " + person.Address + "\n" + "City: " + person.City + "\n" + "PhoneNumber: " + person.PhoneNum + "\n" + "Email: " + person.Email);
+                }
+
+
+            }
+        }
     }
+
 }
