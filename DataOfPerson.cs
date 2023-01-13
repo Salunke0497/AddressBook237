d252f420679fab4f2270e2fe298f563642e791b5
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2023-01-14 00:26:06 +0530

    welcome

--- DataOfPerson.cs
+++ DataOfPerson.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AddressBook
 {
-    class DataOfPerson
+    public class DataOfPerson
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
